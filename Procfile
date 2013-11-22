60cbacca81885cd3a1a2e4b886366a596e6f6e27
Rohit Mittal <rohit.mittal.2006@gmail.com>
2013-11-21 23:06:19 -0800

    removed database line from settings.py and changed virtual env name in gitignore

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web: gunicorn storytime.wsgi
-worker: python storytime/manage.py runserver
 
--- storytime/settings.py
+++ storytime/settings.py
@@ -1,8 +1,5 @@
 # Django settings for storytime project.
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -137,6 +134,7 @@ INSTALLED_APPS = (
     'south',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
+    'dj_static',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
